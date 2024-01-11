67dcf47880e837168d61b94d5fd2c5705827127d
Nishant Pai <nishantpai@Nishants-MacBook-Pro.local>
2024-01-11 05:46:57 +0530

    update background js

--- src/background.js
+++ src/background.js
@@ -13,7 +13,7 @@ chrome.storage.sync.set({
 });
 
 
-chrome.browserAction.onClicked.addListener(function (id, bookmark) {
+chrome.action.onClicked.addListener(function (id, bookmark) {
 		// get tab query
 		var queryInfo = {
 		active: true,
@@ -38,14 +38,14 @@ chrome.browserAction.onClicked.addListener(function (id, bookmark) {
 
 //update icon when you click icon
 var toggle = true;
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
 	toggle = !toggle;
 	if (toggle) {
-		chrome.browserAction.setIcon({ path: "icons/icon_before.png", tabId: tab.id });
+		chrome.action.setIcon({ path: "icons/icon_before.png", tabId: tab.id });
 		chrome.tabs.executeScript(tab.id, { file: "SCRIPT.user.js" });
 	}
 	else {
-		chrome.browserAction.setIcon({ path: "icons/icon_after.png", tabId: tab.id });
+		chrome.action.setIcon({ path: "icons/icon_after.png", tabId: tab.id });
 		chrome.tabs.executeScript(tab.id, { code: "alert()" });
 	}
 });
